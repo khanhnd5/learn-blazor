2929aa83ed617d3a5ccfb19febc248c9e3c7e3f5
“Khanh <khanhnd95.ptit@gmail.com>
2023-06-09 16:17:31 +0700

    Implement View Employee page

---
+++ BlazorWeb/BlazorWebDemo/Client/Pages/EmployeeDetailsBase.cs
@@ -0,0 +1,23 @@
+﻿using BlazorWebDemo.Server.Services;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebDemo.Client.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        public Employee Employee { get; set; } = new Employee();
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Id = Id ?? "1";
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
--- BlazorWeb/BlazorWebDemo/Client/Services/IEmployeeService.cs
+++ BlazorWeb/BlazorWebDemo/Client/Services/IEmployeeService.cs
@@ -5,5 +5,6 @@ namespace BlazorWebDemo.Server.Services
     public interface IEmployeeService
     {
         Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int id);
     }
 }
