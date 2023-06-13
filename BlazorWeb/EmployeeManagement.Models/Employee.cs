40fe9879e4eae5a31a4014ce5722b1fde6634100
“Khanh <khanhnd95.ptit@gmail.com>
2023-06-13 11:32:54 +0700

    Create Form validation

--- BlazorWeb/EmployeeManagement.Models/Employee.cs
+++ BlazorWeb/EmployeeManagement.Models/Employee.cs
@@ -10,12 +10,12 @@ namespace EmployeeManagement.Models
     public class Employee
     {  
         public int EmployeeId { get; set; }
-        [Required]
-        [StringLength(100, MinimumLength = 2)]
+        [Required(ErrorMessage = "First Name is mandatory")]
+        [MinLength(2)]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Last Name is mandatory")]
         public string LastName { get; set; }
-        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
